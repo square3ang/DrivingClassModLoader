ccea3c9b2b190f3219688aaf553bf758a7cd1af9
bluecard <sideofsidegd@gmail.com>
2024-07-29 15:17:39 +0900

    just

--- modder.ts
+++ modder.ts
@@ -146,8 +146,8 @@ Il2Cpp.perform(() => {
         var org = this.method("Start").invoke();
         console.log("Called");
         if (assetBundle == null) {
-            fs.writeFileSync(unitycor.class("UnityEngine.Application").method<Il2Cpp.String>("get_persistentDataPath").invoke().content + "/dcmlassets", new DataView(new Int8Array(ab.dcmlassets).buffer));
-            assetBundle = abloadfromfile(Memory.allocUtf8String(unitycor.class("UnityEngine.Application").method<Il2Cpp.String>("get_persistentDataPath").invoke().content + "/dcmlassets"), 0 ,0);
+            fs.writeFileSync(Il2Cpp.application.dataPath + "/dcmlassets", new DataView(new Int8Array(ab.dcmlassets).buffer));
+            assetBundle = abloadfromfile(Memory.allocUtf8String(Il2Cpp.application.dataPath + "/dcmlassets"), 0 ,0);
             console.log("assetbundle loaded");
         }
         EditMainMenu();
