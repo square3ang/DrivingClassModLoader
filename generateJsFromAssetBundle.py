a9ec66c60ee753bba22715a994f1410d6c5b5fba
square3ang <square3ang@gmail.com>
2025-07-12 17:27:25 +0900

    remove asset bundles

--- generateJsFromAssetBundle.py
+++
@@ -1,8 +0,0 @@
-with open("dcmlassets", "rb") as r:
-    with open("dcmlassets.js", "w") as w:
-        string = ""
-        string += "export const dcmlassets = ["
-        for data in r.read():
-            string += str(int(data)) + ", "
-        string += "];"
-        w.write(string)
